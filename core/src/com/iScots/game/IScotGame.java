f12c0722d5e2d5a78ccff67fa5c5eb68776616ef
Cleefoss <cleefoss@macalester.edu>
2015-03-23 20:40:27 -0500

    Updated with adaptations of javascript functions, plus some stuff for future use.

--- core/src/com/iScots/game/IScotGame.java
+++ core/src/com/iScots/game/IScotGame.java
@@ -5,12 +5,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class IScotGame extends Game {
 	public SpriteBatch batch;
-	
+	public long startTime;
+    public long currentTime;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
         Assets.load();
 		setScreen(new GameScreen(this));
+        startTime = System.currentTimeMillis()/1000;
+        currentTime = System.currentTimeMillis()/1000;
 	}
 
 	@Override
