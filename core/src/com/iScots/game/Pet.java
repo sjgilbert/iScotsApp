b0e5d0f8d5fd53b9103117742d673fb91e93a468
Sam <samjgilbert94@gmail.com>
2015-03-25 20:24:42 -0500

    Fixed the update method, added image.

--- core/src/com/iScots/game/Pet.java
+++ core/src/com/iScots/game/Pet.java
@@ -1,39 +1,53 @@
 package com.iScots.game;
 
 
+import com.badlogic.gdx.graphics.Texture;
+
 /**
  * Created by Christopher on 3/23/2015.
  */
 public class Pet {
-    private float hunger;
-    private float happiness;
-    private float tiredness;
+    private int hunger;
+    private int happiness;
+    private int tiredness;
+
+    private Texture petImage;
 
     public Pet() {
         this.hunger = 100;
         this.happiness = 100;
         this.tiredness = 90;
+        this.petImage = Assets.pet5;
     }
 
     public void update(String action) {
         if (action == "sleep") {
-            tiredness += 20;
-            hunger -= 10;
-            happiness -= 10;
+            if (tiredness > 0 && tiredness < 100) { tiredness += 20;}
+            if (hunger >= 0 && hunger <= 100) { hunger -= 10;}
+            if (happiness >= 0 && happiness <= 100) { happiness -= 10;}
         } else if (action == "feed") {
-            tiredness -= 10;
-            hunger += 20;
-            happiness -= 10;
+            if (tiredness >= 0 && tiredness <= 100) { tiredness -= 10;}
+            if (hunger >= 0 && hunger <= 100) { hunger += 10;}
+            if (happiness >= 0 && happiness <= 100) { happiness -= 10;}
         } else if (action == "play") {
-            tiredness -= 10;
-            hunger -= 10;
-            happiness += 20;
+            if (tiredness >= 0 && tiredness <= 100) { tiredness -= 10;}
+            if (hunger >= 0 && hunger <= 100) { hunger -= 10;}
+            if (happiness >= 0 && happiness <= 100) { hunger += 20;}
         }
         else {
             System.out.println("illegal argument");
         }
+        updateState();
     }
 
+        private void updateState() {
+            if (getHappiness() < 100 / 6.0) {setPetImage(Assets.pet0);}
+            else if (getHappiness() < 2 * 100 / 6) {setPetImage(Assets.pet1);}
+            else if (getHappiness() < 3 * 100 / 6.0) {setPetImage(Assets.pet2);}
+            else if (getHappiness() < 4 * 100 / 6.0) {setPetImage(Assets.pet3);}
+            else if (getHappiness() < 5 * 100 / 6.0) {setPetImage(Assets.pet4);}
+            else if (getHappiness() <= 6 * 100 / 6.0) {setPetImage(Assets.pet5);}
+        }
 //    public void doNothing(){
 //        tiredness = tiredness - 10;
 //        hunger =  hunger - 10;
@@ -50,5 +64,9 @@ public class Pet {
 
     public float getTiredness(){
         return tiredness;
+    }
+
+    private void setPetImage(Texture texture) {
+        petImage = texture;
     }
 }
